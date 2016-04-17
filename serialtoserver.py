7fe8980157fa56128e6c71ca1709011c05644a79
Rupesh Chinta <rupesh.ch97@gmail.com>
2016-04-17 11:15:55 -0400

    more edits

--- serialtoserver.py
+++ serialtoserver.py
@@ -8,14 +8,20 @@ temp = 0
 sound = 0
 json = ''
 location = "bcc"
-with serial.Serial('/dev/ttyS0', 9600, timeout=2) as ser:
-	line = ser.readline()
-	if line[0] == 'l'
-		light = int(line[2:])
-	elif line[0] == 't'
-		temp = int(line[2:])
-	elif line[0] == 's':
-		sound = int(line[2:])
-	count = (count + 1 )% 3
-	if not count:
-		json = json.dumps({"light":light,"temperature":temp,"sound":sound,"location":location})
+with serial.Serial('/dev/ttyACM0', 9600, timeout=2) as ser:
+	while True:
+		try:
+			line = ser.readline()
+			if line[0] == 'l':
+				light = int(line[2:-4])
+			elif line[0] == 't':
+				temp = int(line[2:-4])
+			elif line[0] == 's':
+				sound = int(line[2:-4])
+			count = (count + 1 )% 3
+			if not count:
+				json = json.dumps({"light":light,"temperature":temp,"sound":sound,"location":location})
+				print(json)
+		except:
+			print("not working")s
+			continue
