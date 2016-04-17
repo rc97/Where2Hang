bcb7d606a70c423a52f06d792525b3544215c853
Rupesh Chinta <rupesh.ch97@gmail.com>
2016-04-17 08:14:28 -0400

    added more to the serial python thing, needs a url

--- serialtoserver.py
+++ serialtoserver.py
@@ -1,7 +1,21 @@
 import serial
+import json
 
+url = "asdfasdf"
 count = 0
+light = 0
+temp = 0
+sound = 0
+json = ''
+location = "bcc"
 with serial.Serial('/dev/ttyS0', 9600, timeout=2) as ser:
 	line = ser.readline()
-	count%3 something
-	count+=1
+	if count%3 == 0:
+		light = int(line)
+	elif count%3 == 1:
+		temp = int(line)
+	else:
+		sound = int(line)
+	count = (count + 1 )% 3
+	if not count:
+		json = json.dumps({"light": light, "temperature": temp, "sound": sound, "location", location})
